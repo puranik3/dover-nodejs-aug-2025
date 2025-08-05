2c70a68b14ca60cae6e6bf33621c9ea5cc633b3c
Prashanth Puranik <puranik@digdeeper.in>
2025-08-05 10:42:07 +0530

    promise chaining

---
+++ demos/10-callback-hell.js
@@ -0,0 +1,34 @@
+function sum( x, y, callback ) {
+    setTimeout(
+        function() {
+            callback( x + y );
+        },
+        3000
+    );
+
+    // return undefined;
+}
+
+sum(
+    12,
+    13,
+    ( result1 ) => {
+        console.log( 'result1 = ', result1 );
+
+        sum(
+            result1,
+            14,
+            ( result2 ) => {
+                console.log( 'result2 = ', result2 );
+
+                sum(
+                    result2,
+                    15,
+                    ( result3 ) => {
+                        console.log( 'result3 = ', result3 );
+                    }
+                )
+            }
+        );
+    }
+);
---
+++ demos/11-promise.js
@@ -0,0 +1,44 @@
+function sum( x, y ) {
+    return new Promise(
+        ( resolve, reject ) => {
+            setTimeout(
+                () => {
+                    // We communicate to the Promise object, the result
+                    resolve( x + y );
+                },
+                3000
+            );
+        }
+    );
+}
+
+// Hey Promise! When you get the result, call the function passed to then()
+sum( 12, 13 )
+    .then(
+        ( result1 ) => {
+            console.log( "result1 = ", result1 );
+
+            // sum( result1, 14 )
+            //     .then(
+            //         ( result2 ) => {
+            //             console.log( "result2 = ", result2 );
+            //         }
+            //     );
+
+            return sum( result1, 14 );
+        }
+    )
+    .then(
+        ( result2 ) => {
+            console.log( "result2 = ", result2 );
+
+            return sum( result2, 15 );
+        }
+    )
+    .then(
+        ( result3 ) => {
+            console.log( "result3 = ", result3 );
+        }
+    );
+
+console.log( 'end of script' );
