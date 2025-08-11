06a830c24dffe3abc1dba3a8f3c41e87cc843220
Prashanth Puranik <puranik@digdeeper.in>
2025-08-11 12:16:12 +0530

    STEP 10: Index controllers added

--- demos/02-express/workshops-app/src/controllers/index.controller.js
+++ demos/02-express/workshops-app/src/controllers/index.controller.js
@@ -1,3 +1,16 @@
 // you need to move the controller functions here, export them, and use in index.route.js file
 
-// your code...
+const getIndex = ( req, res ) => {
+    res.write( 'This is the workshops app. It serves details of workshops happening nearby.' );
+    res.end();
+};
+
+const getHome = ( req, res ) => {
+    // tell the browser to make request to / instead. On receiving this response, the browser makes a new request to /
+    res.redirect( '/' );
+};
+
+module.exports = {
+    getIndex,
+    getHome
+};
--- demos/02-express/workshops-app/src/routes/index.route.js
+++ demos/02-express/workshops-app/src/routes/index.route.js
@@ -1,16 +1,10 @@
 const express = require( 'express' );
+const controllers = require( '../controllers/index.controller' );
 
 const router = express.Router();
 
 // setting up the routes
-router.get( '/', ( req, res ) => {
-    res.write( 'This is the workshops app. It serves details of workshops happening nearby.' );
-    res.end();
-});
-
-router.get( '/home', ( req, res ) => {
-    // tell the browser to make request to / instead. On receiving this response, the browser makes a new request to /
-    res.redirect( '/' );
-});
+router.get( '/', controllers.getIndex );
+router.get( '/home', controllers.getHome);
 
 module.exports = router;
