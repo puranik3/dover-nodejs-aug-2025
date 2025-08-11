e3b43d0634e417902b0d4228789937b97cd4757b
Prashanth Puranik <puranik@digdeeper.in>
2025-08-11 12:07:40 +0530

    Step 10: Refactoring to create controllers

---
+++ demos/02-express/workshops-app/src/controllers/workshops.controller.js
@@ -0,0 +1,20 @@
+const workshops = require( '../data/workshops.json' );
+
+const getWorkshops = ( req, res, next ) => {
+    res.json( workshops );
+};
+
+const postWorkshop = ( req, res, next ) => {
+    const newWorkshop = req.body;
+
+    newWorkshop.id = nextId;
+    ++nextId;
+    workshops.push( newWorkshop );
+
+    res.status( 201 ).json( newWorkshop );
+};
+
+module.exports = {
+    getWorkshops,
+    postWorkshop
+};
--- demos/02-express/workshops-app/src/routes/workshops.route.js
+++ demos/02-express/workshops-app/src/routes/workshops.route.js
@@ -1,22 +1,12 @@
 const express = require( 'express' );
-const workshops = require( '../data/workshops.json' );
+const controllers = require( '../controllers/workshops.controller' );
 
 let nextId = 13;
 
 const router = express.Router();
 
 router.route( '/' )
-    .get(( req, res, next ) => {
-        res.json( workshops );
-    })
-    .post(( req, res, next ) => {
-        const newWorkshop = req.body;
-
-        newWorkshop.id = nextId;
-        ++nextId;
-        workshops.push( newWorkshop );
-
-        res.status( 201 ).json( newWorkshop );
-    });
+    .get( controllers.getWorkshops )
+    .post( controllers.postWorkshop );
 
 module.exports = router;
